205b2f15d2ed46ba814683654ef7c9e5ba761cd8
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2022-09-26 22:14:00 -0800

    init

---
+++ trends/trends/trends.go
@@ -0,0 +1,7 @@
+package trends
+
+// TODO:
+// - [ ] Add a way to get the top 10 topics discussed on our club's Dicord server for a given time period
+// - [ ] https://github.com/diamondburned/arikawa/blob/331ec59dec98b70a95d99d26644e0e82289c64df/api/message.go#L36
+// - [ ] https://github.com/fluhus/gostuff/blob/7507ab1234a916139f09c613668759580182af1b/nlp/lda-tool/main.go#L38
+// - [ ] https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows#schedule
